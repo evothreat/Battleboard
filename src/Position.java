d0086329b50f2bb959c45c8ae7c8ef3c60c501a5
Adam <cihasev@uni-bremen.de>
2022-04-07 00:31:43 +0200

    feat: Position

---
+++ src/Position.java
@@ -0,0 +1,22 @@
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setPosition(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
