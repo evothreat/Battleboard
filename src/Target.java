8093f6515761c1f8da042db201b7c4d2b9468c90
Adam <cihasev@uni-bremen.de>
2022-04-08 07:19:31 +0200

    refactor: Direction enum integrated

--- src/Target.java
+++ src/Target.java
@@ -8,26 +8,26 @@ public class Target {
 
     static List<Position> getTargetsInCross(Board board, Position position, Color color) {
         return Stream.of(
-                getTargetsInDirection(board, position, color, 0, 1),
-                getTargetsInDirection(board, position, color, 1, 0),
-                getTargetsInDirection(board, position, color, 0, -1),
-                getTargetsInDirection(board, position, color, -1, 0)
+                getTargetsInDirection(board, position, color, Direction.N),
+                getTargetsInDirection(board, position, color, Direction.S),
+                getTargetsInDirection(board, position, color, Direction.W),
+                getTargetsInDirection(board, position, color, Direction.E)
         ).flatMap(Collection::stream).collect(Collectors.toList());
     }
 
     static List<Position> getTargetsInDiagonal(Board board, Position position, Color color) {
         return Stream.of(
-                getTargetsInDirection(board, position, color, 1, 1),
-                getTargetsInDirection(board, position, color, 1, -1),
-                getTargetsInDirection(board, position, color, -1, -1),
-                getTargetsInDirection(board, position, color, -1, 1)
+                getTargetsInDirection(board, position, color, Direction.NE),
+                getTargetsInDirection(board, position, color, Direction.SE),
+                getTargetsInDirection(board, position, color, Direction.SW),
+                getTargetsInDirection(board, position, color, Direction.NW)
         ).flatMap(Collection::stream).collect(Collectors.toList());
     }
 
-    static public List<Position> getTargetsInDirection(Board board, Position position, Color color, int dirX, int dirY) {
+    static public List<Position> getTargetsInDirection(Board board, Position position, Color color, Direction direction) {
         List<Position> targets = new ArrayList<>();
-        int currX = position.getX() + dirX;
-        int currY = position.getY() + dirY;
+        int currX = position.getX() + direction.getX();
+        int currY = position.getY() + direction.getY();
         while (currX >= 0 && currX < 8 && currY >= 0 && currY < 8) {
             targets.add(new Position(currX, currY));
             // our piece or enemy's piece in the way
@@ -38,8 +38,8 @@ public class Target {
                 }
                 break;
             }
-            currX += dirX;
-            currY += dirY;
+            currX += direction.getX();
+            currY += direction.getY();
         }
         return targets;
     }
