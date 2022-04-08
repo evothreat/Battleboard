5c6d73367052d259c969f74ccc6b07b5d4ecea21
Adam <cihasev@uni-bremen.de>
2022-04-08 07:10:58 +0200

    feat: toggle(), isBlack() & isWhite()

--- src/Color.java
+++ src/Color.java
@@ -1,4 +1,16 @@
 public enum Color {
     BLACK,
-    WHITE
+    WHITE;
+
+    public Color toggle() {
+        return this == BLACK ? WHITE : BLACK;
+    }
+
+    public boolean isBlack() {
+        return this == BLACK;
+    }
+
+    public boolean isWhite() {
+        return this == WHITE;
+    }
 }
