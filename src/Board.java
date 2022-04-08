018d6aa609d5af603ef52b9ef0dde383d62207ec
Adam <cihasev@uni-bremen.de>
2022-04-08 07:11:46 +0200

    refactor: method name changes feat: setPieceAt()

--- src/Board.java
+++ src/Board.java
@@ -2,18 +2,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
+
     private final Piece[][] state;
 
     public Board() {
         state = new Piece[8][8];
     }
 
-    public Piece[][] getState() {
-        return state;
+    public Piece getPieceAt(final int x, final int y) {
+        return state[x][y];
     }
 
     public Piece getPieceAt(final Position position) {
-        return state[position.getX()][position.getY()];
+            return state[position.getX()][position.getY()];
+    }
+
+    public void setPieceAt(final Position position, Piece piece) {
+        state[position.getX()][position.getY()] = piece;
     }
 
     public void move(final Position src, final Position dst) {
@@ -29,16 +34,16 @@ public class Board {
         return MateType.NONE;
     }
 
-    public List<Position> getLegalMoves(final Color color) {
-        List<Position> legalMoves = new ArrayList<>();
+    public List<Position> getValidTargets(final Color color) {
+        List<Position> moves = new ArrayList<>();
         for (int x = 0; x < 8; x++) {
             for (int y = 0; y < 8; y++) {
                 Piece p = state[x][y];
                 if (p != null && p.getColor() == color) {
-                    legalMoves.addAll(p.getLegalMoves(this, new Position(x, y)));
+                    moves.addAll(p.getValidTargets(this, new Position(x, y)));
                 }
             }
         }
-        return legalMoves;
+        return moves;
     }
 }
