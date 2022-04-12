7759adbf4007fa55d2565820d866a26cc89bd925
cihashev <cihashev99@gmail.com>
2022-04-12 08:11:12 +0200

    feat: king avoids enemy king

--- src/King.java
+++ src/King.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class King extends Piece {
 
@@ -9,28 +10,21 @@ public class King extends Piece {
 
     @Override
     List<Square> getValidTargets(Board board, Square square) {
-        int x = square.getX();
-        int y = square.getY();
-        List<Square> targets = new ArrayList<>();
-        targets.add(board.getSquareAt(x - 1, y + 1));
-        targets.add(board.getSquareAt(x, y + 1));
-        targets.add(board.getSquareAt(x + 1, y + 1));
-        targets.add(board.getSquareAt(x + 1, y));
-        targets.add(board.getSquareAt(x + 1, y - 1));
-        targets.add(board.getSquareAt(x, y - 1));
-        targets.add(board.getSquareAt(x - 1, y - 1));
-        targets.add(board.getSquareAt(x - 1, y));
-
-        targets.removeIf(sq -> sq == null || sq.isSettled() && hasSameColor(sq.getPiece()));
+        List<Square> targets = getTargets(board, square);
+        targets.removeIf(sq -> sq.isSettled() && hasSameColor(sq.getPiece()));
 
         boolean w = getColor().isWhite();
         Direction pawnDir = w ? Direction.E : Direction.W;
 
-        for (int i = 0; i < 8 && !targets.isEmpty(); i++) {             // isEmpty() for optimization
-            for (int j = 0; j < 8 && !targets.isEmpty(); j++) {
+        for (int i = 0; i < 8 && !targets.isEmpty(); i++) {                                 // isEmpty() for optimization
+            for (int j = 0; j < 8; j++) {
                 Square src = board.getSquareAt(i, j);
                 Piece p = src.getPiece();
-                if (!src.isSettled() || hasSameColor(p) || p.isKing()) continue;
+                if (p == null || hasSameColor(p)) continue;
+                if (p.isKing()) {
+                    getTargets(board, src).forEach(targets::remove);
+                    continue;
+                }
                 List<Square> enemyTargets = p.getValidTargets(board, src);
                 if (p.isPawn()) {
                     enemyTargets.removeIf(dst -> Direction.from2Points(src.getX(), src.getY(), dst.getX(), dst.getY()) == pawnDir);
@@ -42,6 +36,22 @@ public class King extends Piece {
                 });
             }
         }
+        return targets;
+    }
+
+    private List<Square> getTargets(Board board, Square square) {
+        int x = square.getX();
+        int y = square.getY();
+        List<Square> targets = new ArrayList<>();
+        targets.add(board.getSquareAt(x - 1, y + 1));
+        targets.add(board.getSquareAt(x, y + 1));
+        targets.add(board.getSquareAt(x + 1, y + 1));
+        targets.add(board.getSquareAt(x + 1, y));
+        targets.add(board.getSquareAt(x + 1, y - 1));
+        targets.add(board.getSquareAt(x, y - 1));
+        targets.add(board.getSquareAt(x - 1, y - 1));
+        targets.add(board.getSquareAt(x - 1, y));
+        targets.removeIf(Objects::isNull);
         return targets;
     }
 }
