b6f2b4b4cd54589a44eed2705a3bc7a7021ab867
cyberk1ng <cihashev99@gmail.com>
2022-04-19 02:07:19 +0200

    refactor: if statements replaced with Math min/max

--- src/Minimax.java
+++ src/Minimax.java
@@ -7,35 +7,25 @@ public class Minimax {
         if (depth == 0) {
             return board.evaluate();
         }
-        int minValue = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
         for (Move m : board.getValidMoves(null)) {
-
             board.makeMove(m.getSrc(), m.getDst());
-            int value = max(board, depth-1);
+            min = Math.min(max(board, depth-1), min);
             board.restoreMove();
-
-            if (minValue > value) {
-                minValue = value;
-            }
         }
-        return minValue;
+        return min;
     }
 
     static int max(Board board, int depth) {
         if (depth == 0) {
             return board.evaluate();
         }
-        int maxValue = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
         for (Move m : board.getValidMoves(null)) {
-
             board.makeMove(m.getSrc(), m.getDst());
-            int value = min(board, depth-1);
+            max = Math.max(min(board, depth-1), max);
             board.restoreMove();
-
-            if (value > maxValue) {
-                maxValue = value;
-            }
         }
-        return maxValue;
+        return max;
     }
 }
